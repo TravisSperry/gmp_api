ba4b8a843aa3f04a056e8411af2513e1ee2b810e
Travis Sperry <director@mathplusacademy.com>
2018-04-26 00:06:30 -0400

    Fix nil country issue.

--- app/models/ambassador.rb
+++ app/models/ambassador.rb
@@ -5,6 +5,7 @@ class Ambassador < ApplicationRecord
   validates :email, presence: true
 
   def country_name
+    return unless self.country
     country = ISO3166::Country[self.country]
     country.translations[I18n.locale.to_s] || country.name
   end
