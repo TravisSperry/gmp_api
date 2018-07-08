c45a355996268b4f29ae11bcf082f3743bc1d467
Travis Sperry <director@mathplusacademy.com>
2018-07-08 10:14:32 -0400

    Remove pdf to image conversion.

--- app/uploaders/exploding_dots/chapter_resource_uploader.rb
+++ app/uploaders/exploding_dots/chapter_resource_uploader.rb
@@ -10,32 +10,32 @@ module ExplodingDots
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
 
-    version :preview, if: :image? do
-      process :cover
-      process resize_and_pad: [170, 220, '#FFFFFF', ::Magick::CenterGravity]
-      process convert: :jpg
-      process :set_content_type_img
-
-      def full_filename(for_file = model.source.file)
-        super.chomp(File.extname(super)) + '.jpg'
-      end
-    end
-
-    def cover
-      manipulate! do |frame, index|
-        frame if index.zero?
-      end
-    end
-
-    def set_content_type_img(*_args)
-      file.instance_variable_set(:@content_type, 'image/jpeg')
-    end
-
-    protected
-
-    def image?(new_file)
-      return unless new_file.present?
-      new_file.content_type.start_with?('image') || new_file.content_type.include?('pdf')
-    end
+    # version :preview, if: :image? do
+    #   process :cover
+    #   process resize_and_pad: [170, 220, '#FFFFFF', ::Magick::CenterGravity]
+    #   process convert: :jpg
+    #   process :set_content_type_img
+    #
+    #   def full_filename(for_file = model.source.file)
+    #     super.chomp(File.extname(super)) + '.jpg'
+    #   end
+    # end
+    #
+    # def cover
+    #   manipulate! do |frame, index|
+    #     frame if index.zero?
+    #   end
+    # end
+    #
+    # def set_content_type_img(*_args)
+    #   file.instance_variable_set(:@content_type, 'image/jpeg')
+    # end
+    #
+    # protected
+    #
+    # def image?(new_file)
+    #   return unless new_file.present?
+    #   new_file.content_type.start_with?('image') || new_file.content_type.include?('pdf')
+    # end
   end
 end
